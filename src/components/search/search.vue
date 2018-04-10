f6120ee7ab6b3bb39f43cbb6d8307ed0c6ae4c70
jianye338 <278253918@qq.com>
2018-04-10 14:26:57 +0800

    commit int

---
+++ src/components/search/search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    推荐页面a
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    推荐页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'singer'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
